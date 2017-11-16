c82639bda6184d9919f5453bf050c5d059c622c3
Rafael Laboissiere <rafael@laboissiere.net>
2017-11-16 11:53:04 +0100

    test_linkclass.py: Add instructions for running the unit tests

--- test_linkclass.py
+++ test_linkclass.py
@@ -15,6 +15,9 @@
 ## You should have received a copy of the GNU Affero General Public License
 ## along with this program.  If not, see http://www.gnu.org/licenses/.
 
+## For running this test in a standalone-way, run:
+##     (cd .. ; python -Wd -m unittest discover)
+
 import os
 import re
 import sys
