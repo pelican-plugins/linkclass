511ae6de6ffb03c6ae5ed07c29792f65c5410690
Rafael Laboissière <rafael@laboissiere.net>
2025-01-20 07:40:06 +0100

    Remove extra blank line after initial comment

--- pelican/plugins/linkclass/linkclass.py
+++ pelican/plugins/linkclass/linkclass.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see http://www.gnu.org/licenses/.
 
-
 from pelican import signals
 
 from .mdx_linkclass import LC_CONFIG, LinkClassExtension
